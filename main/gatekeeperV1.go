240690f9c938e8234d6af68052dc926fb5110f4e
Mooiman123 <559155@student.fontys.nl>
2025-02-13 13:27:02 +0100

    tweede versie met het formaat : [groet]! Welkom bij Fonteyn Vakantieparken

--- main/gatekeeperV1.go
+++ main/gatekeeperV1.go
@@ -8,11 +8,11 @@ import (
 func main() {
 	hour := time.Now().Hour() // Haal alleen het uur op
 
-	if hour >= 7 && hour < 12 {
+	if hour > 7 && hour < 12 {
 		fmt.Println("Goedemorgen! Welkom bij Fonteyn Vakantieparken!")
-	} else if hour >= 12 && hour < 18 {
+	} else if hour > 12 && hour < 18 {
 		fmt.Println("Goedemiddag! Welkom bij Fonteyn Vakantieparken!")
-	} else if hour >= 18 && hour < 23 {
+	} else if hour > 18 && hour < 23 {
 		fmt.Println("Goedenavond! Welkom bij Fonteyn Vakantieparken!")
 	} else {
 		fmt.Println("Sorry, de parkeerplaats is gesloten")
